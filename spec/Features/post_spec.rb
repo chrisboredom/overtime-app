5ac07f34683a397f9fd9344a361eb52ec119791d
Chris Mangano <chris.mangano@me.com>
2016-12-22 20:06:51 -0500

    Implemented user connection to posts with tests

--- spec/Features/post_spec.rb
+++ spec/Features/post_spec.rb
@@ -17,6 +17,9 @@ RSpec.feature do
 
   describe 'creation' do
     before do
+      user = User.create(email: "test@example.com", password: "password",
+        first_name: "Jon", last_name: "Snow", password_confirmation: "password")
+      login_as(user, :scope => :user)
       visit new_post_path
     end
 
@@ -31,6 +34,14 @@ RSpec.feature do
       click_on "Save"
 
       expect(page).to have_content("Some rationale")
+    end
+
+    it 'will have a user associated with it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
