3d547a94b04f236e61426dfecd706e3236eef545
Chris Mangano <chris.mangano@me.com>
2016-12-30 19:04:18 -0500

    Added new post button to nav bar

--- spec/Features/post_spec.rb
+++ spec/Features/post_spec.rb
@@ -28,6 +28,15 @@ RSpec.feature do
       end
     end
 
+    describe 'new' do
+      it 'has a link from the homepage' do
+        visit root_path
+
+        click_link("new_post_from_nav")
+        expect(page.status_code).to eq(200)
+      end
+    end
+
     describe 'creation' do
       before do
         visit new_post_path
@@ -62,7 +71,7 @@ RSpec.feature do
       it 'can be reached by clicking edit on index page' do
         visit posts_path
 
-        click_link ("edit_#{@post.id}")
+        click_link("edit_#{@post.id}")
         expect(page.status_code).to eq(200)
       end
 
