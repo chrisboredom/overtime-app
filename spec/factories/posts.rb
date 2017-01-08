d1ef9a3652b38783d9c373df552dfbe652965754
Chris Mangano <chris.mangano@me.com>
2017-01-07 21:55:38 -0500

    Implemented new index query for showing users posts they created and hiding others

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,9 +4,16 @@ FactoryGirl.define do
     rationale "Some rationale"
     user
   end
+
   factory :second_post, class: "Post" do
     date Date.yesterday
     rationale "Some more content"
     user
+  end
+
+  factory :post_from_other_user, class: "Post" do
+    date Date.yesterday
+    rationale "Some more content"
+    non_authorized_user
   end
 end
