eeed286b4e2ec4a2bae26a7ee623d17b90aaa45b
Chris Mangano <chris.mangano@me.com>
2017-01-15 13:54:32 -0500

    Implemented initial view code for audit logs

---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature do
+  describe 'AuditLog Feature' do
+    describe 'index' do
+      before do
+        admin_user = FactoryGirl.create(:admin_user)
+        login_as(admin_user, :scope => :user)
+        FactoryGirl.create(:audit_log)
+      end
+      it 'has an index that can be reached' do
+        visit audit_logs_path
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'renders audit log content' do
+        visit audit_logs_path
+        expect(page).to have_content(/SNOW/)
+      end
+    end
+  end
+end
