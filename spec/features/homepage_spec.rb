3c36a5713bde12b636113a09b3967b52c43bd263
Chris Mangano <chris.mangano@me.com>
2017-01-19 19:41:27 -0500

    Built in approval and review buttons for admin homepage

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature do
+  describe 'Homepage' do
+    it 'allows the admin approve posts fro the homepage' do
+      post = FactoryGirl.create(:post)
+      admin_user = FactoryGirl.create(:admin_user)
+      login_as(admin_user, :scope => :user)
+
+      visit root_path
+
+      click_on("approve_#{post.id}")
+
+      expect(post.reload.status).to eq('approved')
+    end
+  end
+end
