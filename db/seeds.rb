818f769be0065999f3a988138e1bb29b12aafe86
Chris Mangano <chris.mangano@me.com>
2017-01-22 12:41:07 -0500

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,14 +15,14 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created"
 
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content Hoodie actually thundercats mixtape, chia master cleanse dreamcatcher literally. Church-key cliche polaroid, chartreuse tilde iPhone freegan bushwick ugh poke pour-over sriracha. Tbh four loko la croix sustainable man braid. Meggings next level salvia YOLO messenger bag whatever glossier, prism williamsburg hashtag thundercats +1. Enamel pin tote bag readymade, vinyl drinking vinegar succulents try-hard sriracha lomo gentrify heirloom hell of slow-carb. Iceland literally heirloom sriracha put a bird on it tumblr VHS forage offal. Kitsch ennui truffaut tbh migas, photo booth tilde succulents fashion axe echo park wolf.", user_id: @user.id, overtime_request: 2.5)
-end
-
-puts "100 posts have been created"
-
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content Hoodie actually thundercats mixtape, chia master cleanse dreamcatcher literally. Church-key cliche polaroid, chartreuse tilde iPhone freegan bushwick ugh poke pour-over sriracha. Tbh four loko la croix sustainable man braid. Meggings next level salvia YOLO messenger bag whatever glossier, prism williamsburg hashtag thundercats +1. Enamel pin tote bag readymade, vinyl drinking vinegar succulents try-hard sriracha lomo gentrify heirloom hell of slow-carb. Iceland literally heirloom sriracha put a bird on it tumblr VHS forage offal. Kitsch ennui truffaut tbh migas, photo booth tilde succulents fashion axe echo park wolf.", user_id: @user.id, overtime_request: 2.5)
+end
+
+puts "100 posts have been created"
