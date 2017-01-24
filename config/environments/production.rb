5caa29b3f3340b7cd32d4994befbcd105a187766
Chris Mangano <chris.mangano@me.com>
2017-01-23 19:08:37 -0500

    Added mailer config requirement to production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.serve_static_assets = true
   config.assets.digest = true
+  config.action_mailer.default_url_options = { :host => 'udemy-overtime-cdm.herokuapp.com' }
 end
